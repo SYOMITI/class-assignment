1326c73e7412dcd95315bbdb8dbd7fdefe110410
SYOMITI <115490512+SYOMITI@users.noreply.github.com>
2024-11-12 18:52:34 +0300

    Create iadmission.cpp

---
+++ iadmission.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int age;
+    cout<<"please enter the age";
+    cin>> age;
+    if(age>4)
+    cout<<"admit to school";
+}
+else{
+    cout<<"declined! minimum age not reached"<<endl;
+    
+
+}
